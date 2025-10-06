473629d882a1faec13e66e71c3459009c1cb2b34
StackOdyssey <davidsmithny01@gmail.com>
2025-10-07 00:31:00 +0100

    writing the script

--- quiz.js
+++ quiz.js
@@ -1,12 +1,19 @@
-function checkAnswer() {
-    const correctAnswer = "4";
-    const selectedOption = document.querySelector('input[name="quiz"]:checked');
-    const userAnswer = selectOption ? selectOption.value : null;
+document.addEventListener("DOMContentLoaded", function () {
 
-    const feedback = document.getElementById("feedback");
-    if (userAnswer === correctAnswer) {
-        feedback.textContent = "Correct! well done."
-    } else {
-        feedback.textContent = "That's incorrect. Try again!";
+    function checkAnswer() {
+        const correctAnswer = "4";
+        const selectedOption = document.querySelector('input[name="quiz"]:checked');
+        const userAnswer = selectOption ? selectOption.value : null;
+
+        const feedback = document.getElementById("feedback");
+        if (userAnswer === correctAnswer) {
+            feedback.textContent = "Correct! well done."
+        } else {
+            feedback.textContent = "That's incorrect. Try again!";
+        }
     }
-}
+
+    const submitButton = document.getElementById("submit-answer");
+    submitButton.addEventListener("click", checkAnswer);
+
+});
