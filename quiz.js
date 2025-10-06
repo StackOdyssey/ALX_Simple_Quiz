c4cf61a0535959bfbaddcd1172e46c17a47b09ef
StackOdyssey <davidsmithny01@gmail.com>
2025-10-07 00:27:16 +0100

    creating the files

---
+++ quiz.js
@@ -0,0 +1,12 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = selectOption ? selectOption.value : null;
+
+    const feedback = document.getElementById("feedback");
+    if (userAnswer === correctAnswer) {
+        feedback.textContent = "Correct! well done."
+    } else {
+        feedback.textContent = "That's incorrect. Try again!";
+    }
+}
